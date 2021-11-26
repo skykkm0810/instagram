c814ca8a3b003c6278059593f247bae4902cb7b1
skykkm0810 <skykkm0810@naver.com>
2021-11-26 17:33:01 +0900

    insta token get

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Instafeed from "./instafeed";
 function App() {
+  var feed = new Instafeed();
   return (
     <div className="App">
       <div id="instagram"></div>
+      <div id="instafeed"></div>
     </div>
   );
 }
